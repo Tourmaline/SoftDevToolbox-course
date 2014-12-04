0cdab81e14eac4f2778cff896c4c88b1293b0918
Anastasia <nastja.kruchinina@gmail.com>
2014-12-04 14:17:47 +0100

    Deleted some files

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anastasia/Desktop/SoftwareDevelopmentToolboxCourse/testing
-# Build directory: /home/anastasia/Desktop/SoftwareDevelopmentToolboxCourse/testing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(unit "/home/anastasia/Desktop/SoftwareDevelopmentToolboxCourse/testing/unit_tests")
